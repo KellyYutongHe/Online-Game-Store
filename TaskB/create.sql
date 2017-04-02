9377e31abad498416265b470c63975a6d1e009e8
Kelly (Yutong) He <yhe29@u.rochester.edu>
2017-04-02 19:06:15 -0400

    merge

--- TaskB/create.sql
+++ TaskB/create.sql
@@ -1,12 +1,13 @@
-drop table if exists customer;
-drop table if exists employee;
-drop table if exists department;
-drop table if exists company;
-drop table if exists transaction;
-drop table if exists product;
 drop table if exists game;
 drop table if exists console;
 drop table if exists accessory;
+drop table if exists transaction;
+drop table if exists product;
+drop table if exists department;
+drop table if exists company;
+drop table if exists customer;
+drop table if exists employee;
+
 create table customer(
   id int primary key,
   name varchar(30),
@@ -15,7 +16,7 @@ create table customer(
   points int,
   username varchar(30),
   hpasswd varchar(20),
-  privilege int
+  privilege varchar(30)
 );
 
 create table department(
@@ -37,20 +38,20 @@ create table employee(
 
 create table company(
   id int primary key,
-  name varchar(30),
+  name varchar(50) unique,
   country varchar(20)
 );
 
 create table product(
   id int primary key,
   name varchar(30),
-  companyID int default 0,
+  companyID varchar(50),
   price decimal(10,2),
   stock int,
   sales int,
   rating float,
   DID int default 1,
-  foreign key(companyID) references company(id)
+  foreign key(companyID) references company(name)
   on update cascade on delete set null,
   foreign key(DID) references department(id)
   on update cascade on delete set null
