5e6603ddb6e3188448a9e5a85d2e0695707313a3
Kelly (Yutong) He <yhe29@u.rochester.edu>
2017-05-21 19:08:31 -0400

    Update db_setup.php

--- ms4/db_setup.php
+++ ms4/db_setup.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "yhe29";
-$password = "HYTame360230!";
+$password = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
